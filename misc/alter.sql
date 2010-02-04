d034fd8f5b229155192afc6cf30c06b43a1d7f66
gggami <gggami@livedoor.com>
2010-02-04 09:53:00 +0900

    rm

--- misc/alter.sql
+++
@@ -1,8 +0,0 @@
-rename table msg to messages;
-rename table special_msg to special_messages;
-
-alter table messages add column weight tinyint not null default 1 after keyword;
-alter table messages add column weight tinyint not null default 1 after keyword;
-
-alter table messages add index weight_idx(weight);
-alter table special_messages add index weight_idx(weight);
